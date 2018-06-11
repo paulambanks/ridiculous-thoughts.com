7c55a288b7af58de93065b4762d40fc5984addad
paulambanks <bpaula92@gmail.com>
2018-06-11 13:20:50 +0100

    Created Custom User Model, user views and forms

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # First/last name is not a global-friendly pattern
+    name = models.CharField(blank=True, max_length=255)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # Email & Password are required by default.
+
+    def __str__(self):
+        return self.email
