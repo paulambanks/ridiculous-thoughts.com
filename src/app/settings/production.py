7c92986021225849ce8558ffc9ab97a1b8c07772
Nigel Banks <nigel.g.banks@gmail.com>
2018-10-01 21:01:52 +0100

    Allow for remote deployment with docker-machine.

--- src/app/settings/production.py
+++ src/app/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['web', 'localhost']
+ALLOWED_HOSTS = ['web', 'ridiculous-thoughts.com']
 
 DATABASES = {
     'default': {
