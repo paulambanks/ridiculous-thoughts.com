6a3a5e5ee68b9ca357e59709ecc73a79007f6515
Nigel Banks <nigel.g.banks@gmail.com>
2018-10-01 22:23:19 +0100

    Simplify local development

--- src/cmd.sh
+++ src/cmd.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -ex
-python wait-for-database.py
+if [ "${DJANGO_SETTINGS_MODULE}" ==  "app.settings.production" ]; then
+  python wait-for-database.py
+fi
 python manage.py makemigrations accounts
 python manage.py migrate accounts
 python manage.py makemigrations website
