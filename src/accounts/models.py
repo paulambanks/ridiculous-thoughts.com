51d55da5ccf5553081a17c715e74746a44122283
paulambanks <bpaula92@gmail.com>
2018-10-05 16:51:54 +0100

    Fixed inline admin to edit user profile

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -52,7 +52,7 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ['first_name', 'last_name']  # Email & Password are required by default.
 
     def __str__(self):
-        return self.email
+        return str(self.email)
 
     objects = UserManager()
 
@@ -96,7 +96,7 @@ class UserProfile(models.Model):
     )
 
     def __str__(self):
-        return self.user
+        return str(self.user)
 
     @property
     def get_absolute_avatar_url(self):
