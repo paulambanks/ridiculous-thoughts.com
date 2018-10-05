51d47a22d61ca51729a0ffe7624977cd172bfe7d
paulambanks <bpaula92@gmail.com>
2018-10-05 21:13:19 +0100

    fixed email to registered user

--- src/app/settings/production.py
+++ src/app/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web', 'ridiculous-thoughts.com']
+ALLOWED_HOSTS = ['ridiculous-thoughts.com']
 
 DATABASES = {
     'default': {
